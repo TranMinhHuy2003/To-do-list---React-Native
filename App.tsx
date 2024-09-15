f9b96b9e2138908ec452a8fbbcc7b30a791a1d29
TranMinhHuy2003 <bosstran1212@gmail.com>
2024-09-15 11:42:42 +0700

    text input

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState(0);
 
   return (
     <View style={styles.container}>
+      <Text>Name: {name}</Text>
+      <TextInput
+        multiline
+        autoCapitalize="words"
+        onChangeText={(value) => setName(value)}
+        style={{
+          borderColor: "green",
+          borderWidth: 1,
+          width: 200,
+          padding: 15
+        }}
+      >
+      </TextInput>
+
+      <Text>Age: {name}</Text>
+      <TextInput
+        onChangeText={(value) => setAge(value)}
+        style={{
+          borderColor: "green",
+          borderWidth: 1,
+          width: 200,
+          padding: 15
+        }}
+        keyboardType="numeric"
+        maxLength={2}
+      >
+      </TextInput>
+
       <Text style={{ color: "pink", fontSize: 40, fontWeight: "bold" }}>
         count = {count}
       </Text>
